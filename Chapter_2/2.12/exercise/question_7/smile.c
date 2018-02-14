c04e75e7dcd765158dc19e3071c337ef46e495b4
boblee200508 <33594722+boblee200508@users.noreply.github.com>
2018-02-14 12:41:34 +0800

    Smile!

---
+++ Chapter_2/2.12/exercise/question_7/smile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void smile(void);
+int main () {
+    smile();
+    smile();
+    smile();
+    printf("\n");
+    smile();
+    smile();
+    printf("\n");
+    smile();
+    printf("\n");
+}
+void smile(void) {
+    printf("Smile!");
+}
